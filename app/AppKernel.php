e27fc391a304d15fe41e5ecbce0538deed46cb11
Hasan Setiawan <hasan.setiawan@kodefox.com>
2016-05-17 12:53:04 +0700

    remove unused template

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new AppBundle\AppBundle(),
+            new Ais\UserBundle\AisUserBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
 			      new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
         );
